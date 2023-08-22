9a1d932757b8aae6c90e0e139377eff28e2d1195
luisfosg <luisfosgdev@gmail.com>
2023-08-22 17:26:48 -0500

    chore: configurando minify html

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,20 @@
 import { defineConfig } from "vite"
-import pugPlugin from "vite-plugin-pug"
-
-const options = {
-  basedir: './src/index.html',
-  pretty: true
-}
-
-const locals = {
-}
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
-  plugins: [pugPlugin(options, locals)],
+  plugins: [
+    createHtmlPlugin({
+      minify: true,
+      pages: [
+        {
+          filename: 'index.html',
+          template: 'index.html',
+        },
+        {
+          filename: 'about.html',
+          template: 'about.html',
+        }
+      ]
+    })
+  ],
 })
