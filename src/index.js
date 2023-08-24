b6768adfdfb3f8958450dbfb522455480ce8c6f3
luisfosg <luisfosgdev@gmail.com>
2023-08-24 16:16:39 -0500

    feat: agregando carousel

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import 'flowbite'
+
 const $ = (element) => {
     return document.querySelector(element)
 }
@@ -20,30 +22,3 @@ menuClose.addEventListener('click', () => {
     menuOpen.style.display = "block";
     if (title) title.style.display = "block";
 })
-
-document.addEventListener("DOMContentLoaded", function() {
-  const modalButtons = document.querySelectorAll("[data-modal-toggle]");
-  const closeModalButtons = document.querySelectorAll("[data-modal-hide]");
-
-  modalButtons.forEach(button => {
-      button.addEventListener("click", () => {
-          const modalTargetId = button.getAttribute("data-modal-toggle");
-          const modalTarget = document.getElementById(modalTargetId);
-
-          modalTarget.classList.remove("hidden");
-          modalTarget.classList.add("flex");
-          modalTarget.setAttribute("aria-hidden", "false");
-      });
-  });
-
-  closeModalButtons.forEach(button => {
-      button.addEventListener("click", () => {
-          const modalTargetId = button.getAttribute("data-modal-hide");
-          const modalTarget = document.getElementById(modalTargetId);
-
-          modalTarget.classList.add("hidden");
-          modalTarget.classList.remove("flex");
-          modalTarget.setAttribute("aria-hidden", "true");
-      });
-  });
-});
